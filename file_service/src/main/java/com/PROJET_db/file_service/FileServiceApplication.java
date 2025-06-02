643e5dec7558cfe9729355aacef3f049c2233142
lisefozing <fozinglise@gmail.com>
2025-06-02 15:45:21 +0100

    le micro-service de gestion de fichier

---
+++ file_service/src/main/java/com/PROJET_db/file_service/FileServiceApplication.java
@@ -0,0 +1,18 @@
+package com.PROJET_db.file_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class FileServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileServiceApplication.class, args);
+	}
+
+}
+
+
+
