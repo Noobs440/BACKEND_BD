83dddaea12ce46f84321f8c84595f00b28c0d6c2
A-LPELAP <damepelap@gmail.com>
2025-06-02 18:04:30 +0200

    premiere version de la configuration de l'API gateway

---
+++ gateway_bd/src/main/java/com/example/gateway_bd/GatewayBdApplication.java
@@ -0,0 +1,19 @@
+package com.example.gateway_bd;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableAutoConfiguration
+@SpringBootApplication
+public class GatewayBdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayBdApplication.class, args);
+	}
+
+}
+
+
+
+
