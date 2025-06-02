528c1e48e29fabc34d9fb012592f181a6ab1fbe0
Noobs440 <arieldoubissi330@gmail.com>
2025-06-03 01:39:34 +0200

    etape precedente

--- academic_service/src/test/java/com/projectdb/academic_service/AcademicServiceApplicationTests.java
+++ academic_service/src/test/java/com/projectdb/academic_service/AcademicServiceApplicationTests.java
@@ -3,7 +3,7 @@ package com.projectdb.academic_service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.profiles.active=test")
 class AcademicServiceApplicationTests {
 
 	@Test
